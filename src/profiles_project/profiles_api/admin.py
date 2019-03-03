532a6d8c35f16d5eff33fad9fa32fa676d2dd8b4
lakhan <lakhanj569@gmail.com>
2019-03-03 13:05:05 +0530

    enabled the django admin for user profiles model

--- src/profiles_project/profiles_api/admin.py
+++ src/profiles_project/profiles_api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+#registering your model in django admin
+admin.site.register(models.UserProfile)
--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -1,94 +1,79 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
-from django.contrib.auth.models import BaseUserManager
 
 
-'''UserProfileManager name should be same as class name'''
 class UserProfileManager(BaseUserManager):
-    '''helps django works with our custom user models'''
+    """Class required by Django for managing our users from the management
+    command.
+    """
+
     def create_user(self, email, name, password=None):
-        '''creates a new user profile'''
+        """Creates a new user with the given detials."""
 
-        '''checks the eamil exist or not'''
+        # Check that the user provided an email.
         if not email:
+            raise ValueError('Users must have an email address.')
 
-            '''pops an error with a xustom message'''
-            raise ValueError("users must  have an email address")
+        # Create a new user object.
+        user = self.model(
+            email=self.normalize_email(email),
+            name=name,
+        )
 
-        '''it converts the email into a standardized format'''
-        email = self.normalize_email(email)
-
-        '''creates a new user profile model as a object'''
-        user = self.model(email=email, name=name)
-
-        '''password setting and it encrypts the password  in hash code '''
+        # Set the users password. We use this to create a password
+        # hash instead of storing it in clear text.
         user.set_password(password)
-
-        '''saves user profiles in same db we created'''
-        user.save(using=self.db)
+        user.save(using=self._db)
 
         return user
 
-    '''here in password dont add none value bocz we need password to access the super user'''
-    def create_super_user(self, email, name, password):
-        '''creates and save a new super user wuth given details'''
-        '''super user has full control in a system like an admin'''
+    def create_superuser(self, email, name, password):
+        """Creates and saves a new superuser with given detials."""
 
-        '''creates an user by calling above function'''
-        user = self.create_user(email, name, password)
+        # Create a new user with the function we created above.
+        user = self.create_user(
+            email,
+            name,
+            password
+        )
 
-        '''to allow access to superuser and as a staff member'''
+        # Make this user an admin.
         user.is_superuser = True
         user.is_staff = True
-
-        user.save(using=self.db)
+        user.save(using=self._db)
 
         return user
 
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
-    """docstring for UserProfile.represents a user profile in our system"""
-    '''unique is used to check the email is unique everytime user signup'''
+    """A user profile in our system."""
 
     email = models.EmailField(max_length=255, unique=True)
-
-    '''for name character is used'''
     name = models.CharField(max_length=255)
-
-    '''it determines whether user is currently active or not'''
     is_active = models.BooleanField(default=True)
-
-    '''bcoz we dont want new members coming in as a staff member'''
     is_staff = models.BooleanField(default=False)
 
-    ''''it  can be used to manage the user profiles objects in django'''
-    '''built in function of django'''
     objects = UserProfileManager()
 
-
     USERNAME_FIELD = 'email'
-
-    '''list of things u wanted to be required'''
-    '''only add name as email is alreaady in required by the system'''
-    REQUIRED_FIELD = ['name']
-
-    '''now create helper functions'''
+    REQUIRED_FIELDS = ['name']
 
     def get_full_name(self):
-        '''used to get users full name'''
+        """
+        Required function so Django knows what to use as the users full name.
+        """
 
-        return self.name
+        self.name
 
     def get_short_name(self):
-        '''used to get  users short name, u can also do it for getting last name and first name'''
+        """
+        Required function so Django knows what to use as the users short name.
+        """
 
-        return self.name
+        self.name
 
     def __str__(self):
-        '''this is used to when django needs to convert an objects to strings'''
+        """What to show when we output an object as a string."""
 
         return self.email
